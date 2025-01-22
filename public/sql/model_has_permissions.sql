070bf4c2447d19fdd565c45859493dfd94c5890b
Saurab Bhatia <S.Bhatiaodesk@gmail.com>
2025-01-22 12:53:28 +0530

    spatie change

--- public/sql/model_has_permissions.sql
+++ public/sql/model_has_permissions.sql
@@ -3,4 +3,6 @@ VALUES (2, 'Marvel\\Database\\Models\\User', 1),
     (2, 'Marvel\\Database\\Models\\User', 2),
     (2, 'Marvel\\Database\\Models\\User', 4),
     (2, 'Marvel\\Database\\Models\\User', 5),
-    (3, 'Marvel\\Database\\Models\\User', 1);
+    (3, 'Marvel\\Database\\Models\\User', 1),
+    (4, 'Marvel\\Database\\Models\\User', 1),
+    (5, 'Marvel\\Database\\Models\\User', 1);
--- public/sql/model_has_roles.sql
+++ public/sql/model_has_roles.sql
@@ -3,3 +3,5 @@ VALUES (2, 'Marvel\\Database\\Models\\User', 1),
     (4, 'Marvel\\Database\\Models\\User', 2),
     (4, 'Marvel\\Database\\Models\\User', 4),
     (4, 'Marvel\\Database\\Models\\User', 5);
+    (3, 'Marvel\\Database\\Models\\User', 5);
+    (5, 'Marvel\\Database\\Models\\User', 5);

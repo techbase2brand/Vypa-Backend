75933df7842d86fb02126ee352d9825364b698ae
Saurab Bhatia <S.Bhatiaodesk@gmail.com>
2025-01-22 13:22:04 +0530

    change

--- public/sql/model_has_roles.sql
+++ public/sql/model_has_roles.sql
@@ -2,6 +2,6 @@ INSERT INTO `model_has_roles` (`role_id`, `model_type`, `model_id`)
 VALUES (2, 'Marvel\\Database\\Models\\User', 1),
     (4, 'Marvel\\Database\\Models\\User', 2),
     (4, 'Marvel\\Database\\Models\\User', 4),
-    (4, 'Marvel\\Database\\Models\\User', 5);
-    (3, 'Marvel\\Database\\Models\\User', 5);
+    (4, 'Marvel\\Database\\Models\\User', 5),
+    (3, 'Marvel\\Database\\Models\\User', 5),
     (5, 'Marvel\\Database\\Models\\User', 5);

d92ff03b2255252d11c1690a1c95aa763e9fb7cc
Saurab Bhatia <S.Bhatiaodesk@gmail.com>
2025-01-30 19:32:54 +0530

    company_email

--- packages/marvel/src/Mail/CompanyRegisteredMail.php
+++ packages/marvel/src/Mail/CompanyRegisteredMail.php
@@ -5,7 +5,6 @@ namespace Marvel\Mail;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
@@ -34,23 +33,11 @@ class CompanyRegisteredMail extends Mailable
     /**
      * Get the message content definition.
      */
-    public function content(): Content
+
+    public function build()
     {
-        return new Content(
-            view: 'emails.company_registered', // Make sure this Blade template exists
-            with: [
-                'company' => $this->company
-            ]
-        );
+        return $this->markdown('emails.company_registered');
     }
 
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
-     */
-    public function attachments(): array
-    {
-        return [];
-    }
+
 }

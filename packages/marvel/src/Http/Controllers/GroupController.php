1a326836e054f2769afd7292a2a972b0ee584f33
Saurab Bhatia <S.Bhatiaodesk@gmail.com>
2025-02-03 11:28:45 +0530

    dd

--- packages/marvel/src/Http/Controllers/GroupController.php
+++ packages/marvel/src/Http/Controllers/GroupController.php
@@ -128,8 +128,8 @@ class GroupController extends CoreController
     }
     public function budget(Request $request)
     {
-        dd($request->budget);
-        dd($request->date);
+        //dd($request->budget);
+        //dd($request->date);
         dd($request->groups);
 //        {
 //            "budget": 200,
